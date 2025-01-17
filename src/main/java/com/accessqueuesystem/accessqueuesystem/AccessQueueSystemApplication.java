bb1a3b1fedb95b8150b1c97ef1f572b50f9020cb
jaeweon <fhfkfk789@naver.com>
2025-01-17 18:05:35 +0900

    Initial commit

---
+++ src/main/java/com/accessqueuesystem/accessqueuesystem/AccessQueueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.accessqueuesystem.accessqueuesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessQueueSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessQueueSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accessqueuesystem/accessqueuesystem/AccessQueueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accessqueuesystem.accessqueuesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessQueueSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
